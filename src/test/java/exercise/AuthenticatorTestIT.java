8329fb87e61d7e3024f00985e2e8e76a100fbc34
Mazlum <mazlumsert@gmail.com>
2017-04-24 14:25:48 +0200

    Modified the code in UserFacadeTestIT And AuthenticatorTestIT

--- src/test/java/exercise/AuthenticatorTestIT.java
+++ src/test/java/exercise/AuthenticatorTestIT.java
@@ -12,10 +12,16 @@ public class AuthenticatorTestIT  extends AuthenticatorTest {
 
     @Override
     public Authenticator makeAuthenticator() {
-
         Mailer mailer = mock(Mailer.class);
 
+        if(System.getenv("TRAVIS") != null){
+
+        return new Authenticator( new UserFacadeRealDB("pu_mySql_travis_Integration"),mailer);
+
+        }
+
         return new Authenticator(new UserFacadeRealDB("pu_localDB"), mailer);
+
     }
 
 }
--- src/test/java/exercise/UserFacadeTestIT.java
+++ src/test/java/exercise/UserFacadeTestIT.java
@@ -11,10 +11,11 @@ public class UserFacadeTestIT extends UserFacadeTest {
 
     @Override
     public IUserFacade makeUserFacade() {
+
+        if (System.getenv("TRAVIS") != null) {
+            return new UserFacadeRealDB("pu_mySql_travis_Integration");
+        }
         return new UserFacadeRealDB("pu_localDB");
     }
-
-
-
 
 }
